c14f5da4d2d3761dcc549424e7d460827b3d3ba9
Ananta kandel <kandelananta12@gmail.com>
2023-08-07 23:00:41 +0545

    Navbar and routes for sign up and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Route,Routes} from "react-router-dom";
+import Footer from './Components/Footers';
+import Signin from './Components/Signin';
+import Register from './Components/Register';
+import Aboutus from './Components/Aboutus';
 function App() {
   return (
   <>
-  <Navbar />
+ 
+   <Router>
+   <Navbar />
+    <Routes>
+    <Route path="/" element={< Footer/>} />
+      <Route path="/signin" element={<Register/>} />
+      {/* <Route path='/edit' element={<EditExercise/>} /> */}
+      </Routes>
+
+      </Router>
   </>
   );
 }
